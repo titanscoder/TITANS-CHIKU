231ce3c5f6abfbcd74cb1da381afeb75fd8bad98
THE TITAN NETWORK BOTS <86588091+titanscoder@users.noreply.github.com>
2021-07-16 20:32:17 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -97,7 +97,8 @@ buttons = [
 
 
 HELP_STRINGS = """
-ʜɪ.. ɪ'ᴍ [❥𝚌𝓱ᵢ𝓴ᵤ♡ ^_^](https://telegra.ph/file/a2674ccb56045a5b492e3.jpg) 
+ʜɪ.. ɪ'ᴍ [❥𝚌𝓱ᵢ𝓴ᵤ♡](https://telegra.ph/file/a2674ccb56045a5b492e3.jpg) 
+
 CLICK ON THE GIVEN BUTTONS AS BELOW FOR  INFORMATION ABOUT ALL COMMANDS AND MODULES..""""""
 
 yone_IMG = "https://telegra.ph/file/7e61fe06a9c02747249c4.jpg"
