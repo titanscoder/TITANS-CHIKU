d1e46a8e771f9b4f8c9fdf6936fe957b3bed25e5
Elric <85902055+ElricZ@users.noreply.github.com>
2021-08-15 21:41:01 +0530

    Update __google.py

--- YoneRobot/modules/__google.py
+++ YoneRobot/modules/__google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "Search"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
